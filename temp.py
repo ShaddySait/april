2ab8e07d529b93ed813b358cd6874f40001c4819
ShaddySait <shady.sheikh@edu.sait.ca>
2023-10-19 16:04:47 -0600

    step 3-WIP

--- temp.py
+++ temp.py
@@ -5,9 +5,15 @@ def celsius_to_fahrenheit(celsius):
     """convert value given to fahrenheit"""
     fah= celsius *9/5+32
     return fah
+def fahrenheit_to_clesius(fahrenheit):
+    celsius= fahrenheit -32
+    return celsius
 
 
 celsius =25
 fah= (celsius_to_fahrenheit(celsius))
 print(f"{celsius} degress is {fah} Fahrenheit")
+
+celsius= fahrenheit_to_clesius(fah)
+print(f"{fah} degress Fahrenheit is {celsius} celsius")
 
